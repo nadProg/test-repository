3092357c8c105bc9ad344a28a3c6e8d7de586c5c
Eugeniusz Skrobow <skrobov.ev@gmail.com>
2021-03-16 21:11:34 +0700

    Release v.0.4.0
    - Refactor js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-document.forms[0].addEventListener('submit', (evt) => {
-  const name = evt.currentTarget.elements[0];
-  const phone = evt.currentTarget.elements[1];
+document.forms['order-form'].addEventListener('submit', (evt) => {
+  const name = evt.currentTarget.elements.name;
+  const phone = evt.currentTarget.elements.phone;
   evt.preventDefault();
   setTimeout(() => {
     alert(`
       Dear ${name.value}, your order successfully received!
       We will call you on ${phone.value}
       `);
+    alert('Thank you for your order!');
     name.value = '';
     phone.value = '';
   }, 1500);
